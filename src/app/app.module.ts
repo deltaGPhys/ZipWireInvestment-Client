866ee1ea1e692fc6455694c44e35be7a5f183b73
David Trombello <david@zipcoders-MacBook-Pro-96.local>
2019-12-22 23:18:52 -0500

    updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,9 @@ import { LoginComponent } from './login/login.component';
 import { GoalsComponent } from './goals/goals.component';
 import { AccountsComponent } from './accounts/accounts.component';
 import { InvestmentsComponent } from './investments/investments.component';
-<<<<<<< HEAD
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CreateAccountComponent } from './create-account/create-account.component';
-=======
 import { TransactionFormComponent } from './transaction-form/transaction-form.component';
->>>>>>> 8ef4c7dcf7418a8ab45bb08d540d93b8fa889eb0
 
 @NgModule({
   declarations: [
@@ -25,7 +22,6 @@ import { TransactionFormComponent } from './transaction-form/transaction-form.co
     GoalsComponent,
     AccountsComponent,
     InvestmentsComponent,
-<<<<<<< HEAD
     DashboardComponent,
     CreateAccountComponent
 =======
