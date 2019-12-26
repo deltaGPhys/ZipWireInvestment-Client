bb1852707096c6ecb92d3d10727828de18d6c7ac
deltagphys <cty.jgates@gmail.com>
2019-12-26 10:50:07 -0500

    form setup

--- src/app/investments/investment-buy-form/investment-buy-form.component.ts
+++ src/app/investments/investment-buy-form/investment-buy-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Security } from 'src/app/models/Security';
+import { FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-investment-buy-form',
@@ -10,10 +12,29 @@ export class InvestmentBuyFormComponent implements OnInit {
 
   @Input() securities: Security[];
   @Input() account: Account;
+  buyStockForm: FormGroup;
   
-  constructor() { }
+  constructor() { 
+    this.buyStockForm = this.createFormGroup();
+  }
 
   ngOnInit() {
+  }
+
+  createFormGroup() {
+    return new FormGroup({
+      numShares: new FormControl(''),
+      security: new FormControl(''),
+      totalCost: new FormControl('')
+    });
+  }
+
+  revert() {
+    this.buyStockForm.reset();
+  }
+
+  onSubmit() {
+
   }
 
 }
