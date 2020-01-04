7e1c362c2bccebdeaf293350b28024a1fd1dfc34
Charlotte Beale <charlottebeale@zipcoders-MacBook-Pro-6.local>
2020-01-04 08:15:51 -0500

    pulling

--- src/app/accounts/accounts.component.ts
+++ src/app/accounts/accounts.component.ts
@@ -2,11 +2,28 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-accounts',
-  templateUrl: './accounts.component.html',
+  template: `<h2>
+  Welcome {{name}} Account # {{myId}}</h2>
+
+<header>
+    <h1>Account Summary</h1>
+</header>
+<aside>
+
+    <h2>Checking & Savings</h2>
+    <ul>
+        <li><a href="#">Checking</a></li>
+        <li><a href="#">Savings</a></li>
+        <li><a href="#">Goal Account</a></li>
+
+    </ul>
+</aside>`,
+
   styleUrls: ['./accounts.component.css']
 })
 export class AccountsComponent implements OnInit {
-
+public name = "Charlotte Beale";
+public myId = "675472";
   constructor() { }
 
   ngOnInit() {
