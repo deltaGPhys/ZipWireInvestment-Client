79e6586016075d84a975f0dfdfec83c018fdb4fe
David Trombello <david@zipcoders-MacBook-Pro-96.local>
2020-01-09 14:28:57 -0500

    working on update user profile

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -13,6 +13,7 @@ export class UserService {
 
   @Inject(apiUrl) private apiUrl: string;
   private loginUrl : string = apiUrl +"/login";
+  private updateUserProfileUrl: string = apiUrl + "/dashboard/user/update"; 
   currentUser$: BehaviorSubject<any> = new BehaviorSubject([]);
 
   httpOptions = {
@@ -26,7 +27,7 @@ export class UserService {
   verifyUser (email:string, password:string) : Observable<User> {
     let reqData: Object = {"email": email, "password": password};
     return this.http.post<User>(this.loginUrl+"/verify", reqData, this.httpOptions)
-    .pipe(//tap(data => {console.log(data);}),
+    .pipe(tap(data => {console.log(data);}),
     catchError(this.handleError<User>('verification', null)));
   }
 
@@ -39,6 +40,18 @@ export class UserService {
   updateCurrentUser(user : User) {
     console.log("user update in service",user);
     this.currentUser$.next(user);
+  }
+
+  // updateUserProfile(user: User): Observable<User>{
+  //   //console.log(apiUrl);
+  //   //console.log(this.registerUrl);
+  //   return this.http.post<User>(this.registerUrl, user, this.httpOptions).pipe(tap(data => console.log(data)), catchError(this.handleError<User>('addUser')));
+  // }
+  
+  updateUserProfile(user: User) :Observable<User>{
+    //let reqData: Object = {"id": userId, "firstName": firstName, "lastName": lastName};
+    return this.http.post<User>(this.updateUserProfileUrl, user, this.httpOptions)
+        .pipe(tap(data => {console.log(data);catchError(this.handleError<User>('updateProfile', null))}));
   }
 
 /**
--- src/app/user/update-user/update-user.component.ts
+++ src/app/user/update-user/update-user.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/User';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { InvestmentService } from 'src/app/services/investment.service';
 
 @Component({
@@ -12,22 +12,96 @@ import { InvestmentService } from 'src/app/services/investment.service';
 })
 export class UpdateUserComponent implements OnInit {
 
-  updateUser: FormGroup;
   currentUser: User;
+  updateUserForm: FormGroup;
+  private userEmail: string = "";
+  private userFirstName: string = "";
+  private userLastName: string = "";
+  user: User;
+  emailAlreadyTaken : boolean = false;
+  firstNameEmpty : boolean = false;
+  lastNameEmpty : boolean = false;
+  emailEmpty : boolean = false;
+
 
   constructor(private userService : UserService, private router : Router, private investmentService: InvestmentService) {
     this.userService.currentUser$.subscribe(data => {this.currentUser = data; console.log(this.currentUser);});
+    this.updateUserForm = this.createFormGroup();
    }
   
   ngOnInit() {
   }
 
-  update() : void {
-    this.router.navigate(['/user']);
+  createFormGroup() {
+    return new FormGroup({  
+      firstName: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]),
+      lastName: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-Z]+$')]),
+      //email: new FormControl('', [Validators.required,  Validators.email])
+      //password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!_.@$%^&*-]).{8,}$')]),      
+    });
   }
+
+  get firstName() {
+    return this.updateUserForm.get('firstName');
+  } 
+
+  get lastName() {
+    return this.updateUserForm.get('lastName');
+  } 
+
+  get email() {
+    return this.updateUserForm.get('email');
+  } 
+
+  submit(): void {
+    console.log("inside submit");
+    this.userFirstName = this.updateUserForm.controls.firstName.value;
+    this.userLastName = this.updateUserForm.controls.lastName.value;
+    this.currentUser.firstName = this.userFirstName;
+    this.currentUser.lastName = this.userLastName;
+    console.log(this.currentUser);
+    this.userService.updateUserProfile(this.currentUser)
+    this.updateUserForm.reset();
+    this.router.navigate(['/user']);
+
+    // this.userService.updateUserProfile(this.currentUser)
+    // .subscribe(data => {
+    //       this.user = data;
+    //       this.userService.updateCurrentUser(this.user);
+    //       this.updateUserForm.reset();
+    //       //console.log("User: " , this.user);
+    //       this.router.navigate(['/user']);
+    //      });   
+    
+  }
+
+  // submitToChangeEmail() : void {
+  //   console.log("inside submit")
+  //   this.userService.checkEmailAvailability(this.userEmail).subscribe(data => {
+  //     console.log("taken: ", data);
+  //     if(!data){
+  //       this.emailAlreadyTaken = false;
+  //       this.updateProfile();
+  //       this.updateUserForm.reset();
+  //       this.investmentService.toggleDisplay('portfolio');    
+  //     }
+  //     else {
+  //       this.emailAlreadyTaken = true;
+  //     }
+  //   });
+  // }
+  
 
   toggleDisplay(view: string) {
     this.investmentService.toggleDisplay(view);
+  }
+
+  isUserNull(){
+    if (this.currentUser === null) {
+      this.firstNameEmpty = true;
+      this.lastNameEmpty = true;
+      this.emailEmpty = true;
+    }
   }
 
 }
